0dd440ae141b754705546272a9b06273f67ed8d3
Morteza Ahmadi <morinob93@gmail.com>
2020-11-24 12:42:32 -0500

    initial commit

---
+++ runTests.sh
@@ -0,0 +1,22 @@
+runTest() {
+    for i in {1..12}
+    do
+        local filename="T0$i.exe"
+        if [ $i -gt 9 ]
+        then
+            filename="T$i.exe"
+        fi
+
+        if [ $i -eq 1 ]
+        then
+            cmake-build-debug/./src Suite_of_12_Tests/$filename > VMOutput.txt
+        else
+            cmake-build-debug/./src Suite_of_12_Tests/$filename >> VMOutput.txt
+        fi
+    done
+
+    ./aunit VMOutput.txt
+
+}
+
+runTest
