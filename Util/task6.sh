09b2caa4725402dec2e569daf38ce77b1db72f7e
Morteza Ahmadi <morinob93@gmail.com>
2020-12-06 18:14:50 -0500

    add more tests for task 3

--- Util/task6.sh
+++ Util/task6.sh
@@ -1,4 +1,4 @@
-avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p -DonTarget ../HAL/TestInterman.c ../HAL/interman.c ../HAL/hal.c ../HAL/out.c ../BSL/_cout.c ../BSL/_xtoa.c -o TestInterman.o
+avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p -DonTarget ../Tests/TestInterman.c ../HAL/interman.c ../HAL/hal.c ../HAL/out.c ../BSL/_cout.c ../BSL/_xtoa.c -o TestInterman.o
 
 avr-objcopy -O ihex -j .text -j .data TestInterman.o  TestInterman.hex
 
